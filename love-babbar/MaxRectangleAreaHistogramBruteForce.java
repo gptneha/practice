7962a869522c8fdba470faa3b67a813acaeea014
gptneha <neha.gupta@zomato.com>
2021-09-22 15:17:31 +0530

    reverse linked list

--- love-babbar/MaxRectangleAreaHistogramBruteForce.java
+++ love-babbar/MaxRectangleAreaHistogramBruteForce.java
@@ -1,22 +1,31 @@
 class Solution {
+    
     public int largestRectangleArea(int[] heights) {
         
         int maxArea = 0;
         
         for (int i=0 ; i< heights.length; i++) {
             int left = i-1;
+            int leftArea = 0;
+            int rightArea = 0;
             if (i == 0) {
                 left = i;
+                leftArea = 0;
+            } else {
+                leftArea = getAreaLeft(heights, 0, left, heights[i]);
             }
             int right = i+1;
             if (i == heights.length -1) {
                 right = i;
+                rightArea = 0;
+            } else {
+                rightArea = getAreaRight(heights, right, heights.length-1, heights[i]);
             }
-            System.out.println("Area for "+i);
-            System.out.println(getAreaLeft(heights, 0, left, heights[i]) );
-            System.out.println(getAreaRight(heights, 0, left, heights[i]) );
-            int currentArea = getAreaLeft(heights, 0, left, heights[i]) 
-                + getAreaRight(heights, left, heights.length-1, heights[i] ) 
+            // System.out.println("Area for "+i);
+            // System.out.println(leftArea );
+            // System.out.println(rightArea);
+            int currentArea = leftArea
+                +  rightArea
                 + heights[i]*1;
             if (currentArea > maxArea) {
                 maxArea = currentArea;
@@ -27,11 +36,11 @@ class Solution {
     
     public int getAreaLeft(int[] heights, int startIndex, int endIndex, int hight){
         int width = 0;
-        for (int i =startIndex ; i <= endIndex ; i++) {
+        for (int i =endIndex ; i >= startIndex ; i--) {
             if (heights[i] >= hight) {
                 width = width +1;
             } else if (heights[i] < hight) {
-                width = 0;
+                break;
             }
         }
         return width * hight;
@@ -39,11 +48,11 @@ class Solution {
     
     public int getAreaRight(int[] heights, int startIndex, int endIndex, int hight){
         int width = 0;
-        for (int i = endIndex ; i >= startIndex ; i--) {
+        for (int i = startIndex ; i <= endIndex ; i++) {
             if (heights[i] >= hight) {
                 width = width +1;
             } else if (heights[i] < hight) {
-                width = 0;
+                break;
             }
         }
         return width * hight;
